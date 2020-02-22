c5790c48b2bff7c946225c6c3b1bdd843c7d2011
Parth Dobariya <dobariyaparth25192@gmail.com>
2020-02-22 13:30:01 +0900

    Removed unused imports
    Removed unused imports

--- CapstoneProjectStage2/app/src/main/java/app/parth/in/capstoneprojectstage_2/MainActivity.java
+++ CapstoneProjectStage2/app/src/main/java/app/parth/in/capstoneprojectstage_2/MainActivity.java
@@ -1,23 +1,22 @@
 package app.parth.in.capstoneprojectstage_2;
 
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import android.view.View;
+import android.view.Menu;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import com.google.android.material.navigation.NavigationView;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
